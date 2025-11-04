7028a45a4f45a2485f24b7e77a3a86a3c1947217
marckori <marcel.korinek2003@gmail.com>
2025-11-04 14:10:01 +0100

    renamed the function in tools.py and added a return statement

--- tools.py
+++ tools.py
@@ -1,10 +1,10 @@
 import math
 
-def jain_fairness(vehicles, rho):
+def objective_function(vehicles, rho):
     total = sum(v.path_length for v in vehicles)
     squares = sum(v.path_length ** 2 for v in vehicles)
     jain_fairness = (total ** 2) / (len(vehicles) * squares)
 
-    function = total + rho * (1 - jain_fairness)
+    objective = total + rho * (1 - jain_fairness)
 
-    print(jain_fairness)
+    return objective
